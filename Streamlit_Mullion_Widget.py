52d5894ed81ba617ffa279509f1e1f8f1d4feda4
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-03-14 11:53:02 +0000

    Update Streamlit_Mullion_Widget.py

--- Streamlit_Mullion_Widget.py
+++ Streamlit_Mullion_Widget.py
@@ -39,7 +39,7 @@ if not st.session_state["authenticated"]:
 # ---------------------------
 st.set_page_config(page_title="Section Design Tool", layout="wide")
 st.title("Mullion Design Widget - *Beta Version*")
-st.markdown("Time to find that one in a mullion...")
+st.markdown("Find your one in a mullion ❤️")
 
 # ---------------------------
 # Sidebar Controls
